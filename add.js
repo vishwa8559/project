3729811261f47e12153dd4c0293a6022448f0d84
vishwajeet singh <vishwajeet singh>
2017-03-07 11:20:03 +0530

    new file created

---
+++ add.js
@@ -0,0 +1,11 @@
+var num1=5;
+var num2=6;
+function add()
+{if (true) {}
+	 return num1+num2;
+	}
+	add();
+	var result=add()
+	console.log(result);
+		
+	
---
+++ leapyear.js
@@ -0,0 +1,6 @@
+function leapyear(years)
+{
+	if (years % 4==0)
+	{console.log("this is leapyear")}
+	else {console.log("this is not leapyear")}
+}leapyear(2028)
